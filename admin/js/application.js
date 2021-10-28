2427883e5a91dfbae3bca5494e068a94daea738e
jm11116 <74529681+jm11116@users.noreply.github.com>
2021-10-28 17:18:25 +1100

    application.js now actually works

--- admin/js/application.js
+++ admin/js/application.js
@@ -1,11 +1,8 @@
 $(document).ready(function(){
 
-function preloadImage(url)
-{
-    var img = new Image();
-    img.src = url;
-}
-preloadImage("assets/loading.gif");
+//Preload loading spinner:
+var img = new Image();
+img.src = "assets/loading.gif";
 
 //Prevent jQuery caching AJAX calls:
 $.ajaxSetup ({
@@ -15,79 +12,76 @@ $.ajaxSetup ({
 var base_font_size = "1em";
 var bigger_font_size = "1.3em";
 
-//Load section from query string or make a new one:
-
 class Query {
     constructor(){
         this.url = window.location.href;
         this.keyvaluesarray = this.url.split(/\?|\=|\&/g);
     }
-        present = function(){
-            if (this.url.indexOf("?") == "-1"){
-                return false;
-            } else {
-                return true;
+    present(){
+        if (this.url.indexOf("?") == "-1"){
+            return false;
+        } else {
+            return true;
+        }
+    }
+    parts(part){
+        if (this.present() == true){
+            this.array = this.url.split("?");
+            if (part == "url"){
+                return this.array[0];
+            } else if (part == "query"){
+                return this.array[1];
             }
         }
-        parts = function(part){
-            if (this.present() == true){
-                this.array = this.url.split("?");
-                if (part == "url"){
-                    return this.array[0];
-                } else if (part == "query"){
-                    return this.array[1];
-                }
-            }
+    }
+    append(key, value){
+        this.existing = "?" + this.parts("query") + "&";
+        this.combined = this.existing + key + "=" + value;
+        if (this.present() == true){
+            window.history.replaceState("", "", this.combined);
+        } else {
+            window.history.replaceState("", "", "?" + key + "=" + value);
         }
-        append = function(key, value){
-            this.existing = "?" + this.parts("query") + "&";
-            this.combined = this.existing + key + "=" + value;
-            if (this.present() == true){
-                window.history.replaceState("", "", this.combined);
-            } else {
-                window.history.replaceState("", "", "?" + key + "=" + value);
-            }
+    }
+    replace(key, value){
+        window.history.replaceState("", "", "?" + key + "=" + value);
+    }
+    getvaluefromkey(key){
+        if (this.keyvaluesarray.includes(key) == true){
+            this.valuepos = this.keyvaluesarray.indexOf(key) + 1;
+            return this.keyvaluesarray[this.valuepos];
+        } else {
+            return false;
         }
-        replace = function(key, value){
-                window.history.replaceState("", "", "?" + key + "=" + value);
-        }
-        getvaluefromkey = function(key){
-            if (this.keyvaluesarray.includes(key) == true){
-                this.valuepos = this.keyvaluesarray.indexOf(key) + 1;
-                return this.keyvaluesarray[this.valuepos];
-            } else {
-                return false;
-            }
-        }
-        getkeyfromvalue = function(value){
-            if (this.keyvaluesarray.includes(value) == true){
-                this.valuepos = this.keyvaluesarray.indexOf(value) - 1;
-                return this.keyvaluesarray[this.valuepos];
-            } else {
-                return false;
-            } 
-        }
-        loadfromquery = function(){
-            if (this.present() == true && this.getvaluefromkey("section") != false) {
-                this.pagetoload = this.getvaluefromkey("section");
-                console.log(this.pagetoload);
-                $("#main").load(this.pagetoload);
-                $(".section_link").css("text-decoration", "none");
-                this.id = "#" + this.pagetoload.replace(".php", "_link");
-                $(".section_link").css("font-size", base_font_size);
-                $(this.id).css("text-decoration", "underline");
-                $(this.id).css("font-size", bigger_font_size);
-            } else {
-                $("#main").load("home.php");
-                this.replace("section", "home.php");
-                $("#home_link").css("text-decoration", "underline");
-                $("#home_link").css("font-size", bigger_font_size);
-            }
+    }
+    getkeyfromvalue(value){
+        if (this.keyvaluesarray.includes(value) == true){
+            this.valuepos = this.keyvaluesarray.indexOf(value) - 1;
+            return this.keyvaluesarray[this.valuepos];
+        } else {
+            return false;
+        } 
+    }
+    loadfromquery(){
+        if (this.present() == true && this.getvaluefromkey("section") != false) {
+            this.pagetoload = this.getvaluefromkey("section");
+            console.log(this.pagetoload);
+            $("#main").load(this.pagetoload);
+            $(".section_link").css("text-decoration", "none");
+            this.id = "#" + this.pagetoload.replace(".php", "_link");
+            $(".section_link").css("font-size", base_font_size);
+            $(this.id).css("text-decoration", "underline");
+            $(this.id).css("font-size", bigger_font_size);
+        } else {
+            $("#main").load("home.php");
+            this.replace("section", "home.php");
+            $("#home_link").css("text-decoration", "underline");
+            $("#home_link").css("font-size", bigger_font_size);
         }
     }
 }
 query = new Query;
-query.loadfromquery();
+query.loadfromquery(); //Load section from query string or make a new one:
 
 $(".section_link").click(function(){
     var id = $(this).attr("id");
@@ -109,7 +103,6 @@ $(".section_link").click(function(){
                 alert("An error occured: " + xhr.status + " " + xhr.statusText);
             },
             success: function(data){
-                console.log("Section loaded.");
                 $("#main").html(data);
             }
         });
